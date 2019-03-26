959694fcf99c839c52ab457fcff1e15395881826
Jeison Higuita <jeisonhs93@gmail.com>
2019-03-25 19:30:38 -0500

    add section component

---
+++ components/Section.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <section class="section"><slot></slot></section>
+</template>
+
+
+<script>
+export default {
+  props: ['fixed']
+}
+</script>
+
+
+<style scoped>
+.section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100vw;
+  position: relative;
+}
+
+
+@media (min-width: 1080px) {
+  .section {
+    height: 100vh;
+  }
+}
+
+</style>
